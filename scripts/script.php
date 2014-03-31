dc31773146ef1eddc63d1557a513d5431b89b0da
enviii <dkarkar@gmail.com>
2014-03-31 20:35:11 +0000

    added a bit so images and dates are fetched from curl

--- scripts/script.php
+++ scripts/script.php
@@ -24,7 +24,6 @@ error_reporting(-1);
 
 $doc = new DOMDocument();
 $doc->loadHTMLFile("output.html");
-echo "<br><br>";
 $xpath = new DOMXPath($doc);
 
 //get entire div that contains a ton of messy crap
@@ -36,16 +35,18 @@ foreach ($entireText as $text) {
 
 	$explodedText = preg_split('/[\s]+/', $cleanText);
     foreach ($explodedText as $key => $value) {
-    	echo $key." - ".$value."<br>";
+    	//echo $key." - ".$value."<br>";
     }
 }
 
 echo "<br>";
 
 //get h4 with skin champ names
+$skinArr = array();
 $skinName = $xpath->query('//div[@class="field field-name-body field-type-text-with-summary field-label-hidden"]/h4');
 foreach ($skinName as $skin) {
 	//var_dump($skin->textContent);
+	$skinArr[]=$skin->textContent;
 
 	// split with " " so i can unset from explodedTest
     $explodedSkin = explode(" ", $skin->textContent);
@@ -64,6 +65,29 @@ foreach ($skinXprice as $xprice) {
     //printf($tag->textContent);
     //echo $tag->textContent;
 }
+echo "<br>";
+
+//get skin img
+$imgArr = array();
+$skinImg = $xpath->query('//div[@class="field field-name-body field-type-text-with-summary field-label-hidden"]/div[@class="gs-container default-2-col"]/div/span/a/@href');
+foreach ($skinImg as $img) {
+    //var_dump($img->textContent);
+    $imgArr[]=$img->textContent;
+    //printf($tag->textContent);
+    //echo $tag->textContent;
+}
+echo "<br>";
+
+//get skin img
+$champArr = array();
+$champImg = $xpath->query('//div[@class="field field-name-body field-type-text-with-summary field-label-hidden"]/div[@class="gs-container default-3-col"]/div/span/a/img/@src');
+foreach ($champImg as $champ) {
+    //var_dump($champ->textContent);
+    $champArr[]=$champ->textContent;
+    //printf($tag->textContent);
+    //echo $tag->textContent;
+}
+echo "<br>";
 
 //Get rid of the meaningless text. Split and unset it from explodedTest
 $grabText = "Grab these champions and skins on sale for 50% off for a limited time: Skin SalesGive your champions a new look with these skins: RPChampion SalesAdd these champions to your roster: RP RP RP RP RP ";
@@ -84,28 +108,80 @@ foreach ($explodedText as $key => $value) {
 ///////////////////////////////////////
 //////////////////////////////////////
 echo "<br>";
-
-
-printf("Champion: ".$explodedText[6] ." Original Price: ". $explodedText[7] ." Sale Price: ". $explodedText[8]);
-echo "<br>";
-printf("Champion: ".$explodedText[9] ." Original Price: ". $explodedText[10] ." Sale Price: ". $explodedText[11]);
-echo "<br>";
-printf("Champion: ".$explodedText[12] ." Original Price: ". $explodedText[13] ." Sale Price: ". $explodedText[14]);
-
-echo "<br>";
 echo "<br>";
 
-$index = 0;
-foreach ($skinName as $key => $value) {
-	echo "Skin ".$value->textContent." Original Price: ".$explodedText[$index];
-	$index+=1;
-	echo " Sale Price: ".$explodedText[$index];
-	echo "<br>";
-	$index+=1;
-}
+printf("Champion: ".$explodedText[6] ." Original Price: ". $explodedText[7] ." Sale Price: ". $explodedText[8] ." <img height='150' src=".$champArr[0]."> ");
+echo "<br>";
+printf("Champion: ".$explodedText[9] ." Original Price: ". $explodedText[10] ." Sale Price: ". $explodedText[11]." <img height='150' src=".$champArr[1]."> ");
+echo "<br>";
+printf("Champion: ".$explodedText[12] ." Original Price: ". $explodedText[13] ." Sale Price: ". $explodedText[14]." <img height='150' src=".$champArr[2]."> ");
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+printf("Skin: ".$skinArr[0] ." Original Price: ". $explodedText[0] ." Sale Price: ". $explodedText[1] ." <img height='100' src=".$imgArr[0]."> <img height='100' src=".$imgArr[1]."> ");
+echo "<br>";
+echo "<br>";
+echo "<br>";
+printf("Skin: ".$skinArr[1] ." Original Price: ". $explodedText[2] ." Sale Price: ". $explodedText[3] ." <img height='100' src=".$imgArr[2]."> <img height='100' src=".$imgArr[3]."> ");
+echo "<br>";
+echo "<br>";
+echo "<br>";
+printf("Skin: ".$skinArr[2] ." Original Price: ". $explodedText[4] ." Sale Price: ". $explodedText[5] ." <img height='100' src=".$imgArr[4]."> <img height='100' src=".$imgArr[5]."> ");
+
+
+
+
+echo "<br>";
+echo "<br>";
 
 
 
 
 
+
+
+//messy foreach
+
+// $index = 0;
+// foreach ($skinName as $key => $value) {
+// 	echo "Skin ".$value->textContent." Original Price: ".$explodedText[$index];
+// 	echo "<img height='150' src=".$imgArr[$index].">";
+// 	$index+=1;
+// 	echo " Sale Price: ".$explodedText[$index];
+// 	echo "<img height='150' src=".$imgArr[$index].">";
+// 	echo "<br>";
+// 	$index+=1;
+// }
+
+// $index2=0;
+// foreach ($skinName as $key => $value) {
+// 	echo $skinArr[$index2].$explodedText[$index2].$imgArr[$index2];
+// 	$index+=1;
+// 	echo $skinArr[$index2].$explodedText[$index2].$imgArr[$index2];
+// }
+
+
+
+
+/////////////////////////////////////////////////////////
+///////////get start and end date from url///////////////
+
+preg_match("/(?P<text>\.*)sale-(?P<date1>\d{4})-(?P<date2>\d{4})/", "http://na.leagueoflegends.com/en/news/store/sales/champion-and-skin-sale-0325-0328", $results);
+
+
+$startFirst2 = substr($results[2], 0, 2);
+$startTheRest = substr($results[2], 2);
+$endFirst2 = substr($results[3], 0, 2);
+$endTheRest = substr($results[3], 2);
+echo "<br>";
+
+$startDate = date("Y")."-".$startFirst2."-".$startTheRest;
+$endDate = date("Y")."-".$endFirst2."-".$endTheRest;
+
+echo "<br>";
+echo "Start Date: ".$startDate;
+echo "<br>";
+echo "End Date: ".$endDate;
 ?>
